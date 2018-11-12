39a2dbbc149cd644489510c8d008cb93cecc0543
DougAlves <douglas.felipelima18@gmail.com>
2018-11-12 13:38:33 -0300

    Primeiro commit

---
+++ meufiltro.h
@@ -0,0 +1,31 @@
+#ifndef MEUFILTRO_H
+#define MEUFILTRO_H
+
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <stdlib.h>
+#include <time.h>
+#include "imagem.h"
+
+//declare aqui os seus widgets
+GtkWidget *widgetControleNivel;
+GtkWidget *widgetMisturarCanais;
+GtkWidget *comprimento;
+GtkWidget *espessura;
+GtkWidget *aleatoria;
+GtkWidget *retilinea;
+GtkWidget *densidade;
+GtkWidget *orientacaoPinselada;
+
+Imagem filtroPintura(Imagem original);
+Imagem meuFiltro(Imagem origem);
+
+void inicializarWidgetsMeuFiltro();
+void adicionarWidgetsMeuFiltro(GtkWidget *container);
+
+//esta funcao esta implementada em filtroGtk.c, nao precisa alterar
+void funcaoAplicar(GtkWidget *widget, gpointer data);
+void testeWidget(GtkWidget *widget, gpointer data);
+#endif
+
+
