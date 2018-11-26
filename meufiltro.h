6f79f22ab578f697fc22619dd22ab7e299f59445
DougAlves <douglas.felipelima18@gmail.com>
2018-11-26 10:14:53 -0300

    Implementação da função de rotação de amostra da imagem para pintura

--- meufiltro.h
+++ meufiltro.h
@@ -1,6 +1,6 @@
 #ifndef MEUFILTRO_H
 #define MEUFILTRO_H
-
+#include <math.h>
 #include <gtk/gtk.h>
 #include <glib.h>
 #include <stdlib.h>
@@ -14,13 +14,14 @@ GtkWidget *comprimento;
 GtkWidget *espessura;
 GtkWidget *label_comp;
 GtkWidget *label_espe;
+GtkWidget *angulo;
 
 Imagem filtroPintura(Imagem original);
 Imagem meuFiltro(Imagem origem);
 
 void inicializarWidgetsMeuFiltro();
 void adicionarWidgetsMeuFiltro(GtkWidget *container);
-
+int* rotacionar(Imagem original,int i,int j);
 int bordah(Imagem original, int i, int j);
 int bordav(Imagem original, int i, int j);
 Imagem pintar(Imagem original,int i, int j, Imagem resultado);
